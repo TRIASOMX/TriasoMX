3ffe096bb8ee8706318f523c955342a1bec76306
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2026-02-20 14:38:15 -0600

    Cambios a Asphalt Plants, Asphalt Storage Tanks y Bin Units

--- src/components/BinUnits/BinPlanosSection.tsx
+++ src/components/BinUnits/BinPlanosSection.tsx
@@ -209,7 +209,7 @@ const BinPlanosSection = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <div className="h-[70vh] relative flex items-center justify-center bg-bgMain w-full">
+      <div className="h-[80VH] relative flex items-center justify-center bg-bgMain w-full">
         <div
           className="absolute bottom-0 w-full h-4/6 overflow-hidden"
           style={{
