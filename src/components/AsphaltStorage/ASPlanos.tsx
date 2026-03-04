7ae13f8aef916f4fb0ad0c8904c4b8899e0039ff
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2026-03-04 11:54:52 -0600

    Modificaciones a AsphaltStorage

--- src/components/AsphaltStorage/ASPlanos.tsx
+++ src/components/AsphaltStorage/ASPlanos.tsx
@@ -143,7 +143,7 @@ const ASPlanos = () => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <div className="h-[80vh] relative flex items-center justify-center bg-bgMain w-full">
+      <div className="h-[100vh] lg:h-[80vh] relative flex items-center justify-center bg-bgMain w-full">
         <div
           className="absolute bottom-0 w-full h-4/6 overflow-hidden"
           style={{
