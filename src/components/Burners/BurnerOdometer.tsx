13dceb8686c1ac8477c42a5d9ac38f5d22ac371a
EduMA13 <monarrez.edu.isw@unipolidgo.edu.mx>
2026-04-17 10:56:10 -0600

    Página quemadores

---
+++ src/components/Burners/BurnerOdometer.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState, useRef } from "react";
+import Odometer from "react-odometerjs";
+import "odometer/themes/odometer-theme-default.css";
+
+const BurnerOdometer = () => {
+    const sectionRef = useRef<HTMLDivElement | null>(null);
+
+    const [value, setValue] = useState(0);
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
+    const [value5, setValue5] = useState(0);
+    const [value3, setValue3] = useState(0);
+    const [value4, setValue4] = useState(0);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    // Reinicia valores antes de animar
+                    setValue(0);
+                    setValue1(0);
+                    setValue2(0);
+                    setValue5(0);
+                    setValue3(0);
+                    setValue4(0);
+
+
+                    setTimeout(() => {
+                        setValue(6);
+                        setValue3(125);
+
+                        setValue1(1.2);
+                        setValue4(26.8);
+
+                        setValue2(5);
+                        setValue5(100)
+
+                    }, 300); // Pequeño retraso para asegurar reinicio
+                }
+            },
+            {
+                threshold: 0.5, // cuando el 50% sea visible
+            }
+        );
+
+        const current = sectionRef.current;
+        if (current) observer.observe(current);
+
+        return () => {
+            if (current) observer.unobserve(current);
+        };
+    }, []);
+
+    return (
+        <div ref={sectionRef} className="w-full max-w-7xl px-8 mx-auto lg:mt-56 lg:mb-56 md:mt-56 md:mb-56">
+            <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center ">
+                <div className="flex flex-col items-center lg:items-start justify-center gap-10 md:gap-20">
+                    <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+                        <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-start items-baseline w-full">
+                            <Odometer value={value} format="(,ddd).d" duration={2000} />
+                            <h1>-</h1>
+                            <Odometer value={value3} format="(,ddd).d" duration={2000} />
+                            <p className="text-sm font-normal ml-3">millones</p>
+                        </div>
+                        <p className="text-[#4F4F4F]">BTUH/hr <br />
+                            En modelos seleccionados</p>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+                        <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-start items-baseline w-full">
+                            <Odometer value={value1} format="(,ddd).dd" duration={2000} />
+                            <h1>-</h1>
+                            <Odometer value={value4} format="(,ddd).dd" duration={2000} />
+                            <p className="text-sm font-normal ml-3">mil</p>
+                        </div>
+                        <p className="text-[#4F4F4F]">ACFM</p>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center lg:items-start lg:justify-start">
+                        <div className="flex text-6xl lg:text-[5rem] md:text-[5rem] font-normal justify-start items-baseline w-full">
+                            <Odometer value={value2} format="(,ddd).dd" duration={2000} />
+                            <h1>-</h1>
+                            <Odometer value={value5} format="(,ddd).dd" duration={2000} />
+                            <p className="text-sm font-normal ml-3">Hp</p>
+                        </div>
+                        <p className="text-[#4F4F4F]">Capacidad de quemador</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center justify-center mx-auto gap-6 pt-10">
+                    <div className="w-4/6">
+                        <img src="https://placehold.co/600x400/png" alt="Bin Unit Odometer" className=" object-cover rounded-xl" />
+                    </div>
+                    <a
+                        href="#planosBinUnits"
+                        className="
+    group relative inline-flex items-center justify-center
+    px-4 py-2 rounded-xl
+    border border-black
+    text-black font-medium
+    overflow-hidden
+
+    transition-all duration-300 ease-out
+    hover:text-white hover:-translate-y-0.5 hover:shadow-lg
+  "
+                    >
+                        <span
+                            className="
+      absolute inset-0 bg-black
+      translate-y-full
+      transition-transform duration-300 ease-out
+      group-hover:translate-y-0
+    "
+                        />
+                        <span className="relative z-10">
+                            Todos sus características técnicas
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BurnerOdometer;
