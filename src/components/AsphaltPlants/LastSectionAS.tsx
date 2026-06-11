3933e7e9730976d0b90c616732e95341212dac6f
AlanRB08 <alanreyesbueno08@gmail.com>
2026-06-11 13:19:35 -0600

    Updates Landing & AsphaltPlants

--- src/components/AsphaltPlants/LastSectionAS.tsx
+++ src/components/AsphaltPlants/LastSectionAS.tsx
@@ -1,8 +1,8 @@
 import InfoCard from "../../components/unitComponents/InfoCard";
-import img1 from "../../assets/images/AsphaltPlant/Gallery/Fabricación de pugmill.webp"
-import img2 from "../../assets/images/AsphaltPlant/Gallery/Fabricación de tanque con caldera.webp"
-import img3 from "../../assets/images/AsphaltPlant/Gallery/Quemadores en fabricación.webp"
-import img4 from "../../assets/images/AsphaltPlant/Gallery/Soldadura interna de tambor mezclador.webp"
+import img1 from "../../assets/images/AsphaltPlant/Gallery/Fabricación de pugmill.webp";
+import img2 from "../../assets/images/AsphaltPlant/Gallery/Fabricación de tanque con caldera.webp";
+import img3 from "../../assets/images/AsphaltPlant/Gallery/Quemadores en fabricación.webp";
+import img4 from "../../assets/images/AsphaltPlant/Gallery/Soldadura interna de tambor mezclador.webp";
 
 export default function App() {
   return (
@@ -13,34 +13,35 @@ export default function App() {
             id: "A",
             label: "A",
             image: img3.src,
-            title: "Competitive pricing without compromising quality",
+            title: "Precios competitivos sin comprometer la calidad",
             description:
-              "High quality industrial solutions ensuring reliability and durability.",
+              "Soluciones industriales de alta calidad que garantizan confiabilidad y durabilidad.",
           },
           {
             id: "B",
             label: "B",
             image: img2.src,
             title:
-              "In-depth technical expertise for exceptional customer service",
+              "Amplia experiencia técnica para ofrecer un servicio al cliente excepcional",
             description:
-              "Our equipment is designed with cutting-edge technology for maximum performance.",
+              "Nuestro equipo está diseñado con tecnología de vanguardia para lograr el máximo rendimiento.",
           },
           {
             id: "C",
             label: "C",
             image: img1.src,
-            title: "Customization to adapt to specific customer needs",
+            title:
+              "Personalización para adaptarnos a las necesidades específicas de cada cliente",
             description:
-              "We have supplied and installed equipment worldwide with proven results.",
+              "Hemos suministrado e instalado equipos en todo el mundo con resultados comprobados.",
           },
           {
             id: "D",
             label: "D",
             image: img4.src,
-            title: "Immediate spare parts availability",
+            title: "Disponibilidad inmediata de refacciones",
             description:
-              "Dedicated support team to ensure smooth operation and long-term satisfaction.",
+              "Equipo de soporte dedicado para garantizar una operación fluida y satisfacción a largo plazo.",
           },
         ]}
       />
